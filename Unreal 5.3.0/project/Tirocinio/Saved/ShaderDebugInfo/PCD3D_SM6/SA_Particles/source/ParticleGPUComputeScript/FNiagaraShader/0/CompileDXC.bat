5367b9c0feb739826ed2fad6de24f07ef27480f4
Vinetwigs <espositovincenzopio@gmail.com>
2024-04-27 15:03:21 +0200

    Changed visualization to GLSL

--- Unreal 5.3.0/project/Tirocinio/Saved/ShaderDebugInfo/PCD3D_SM6/SA_Particles/source/ParticleGPUComputeScript/FNiagaraShader/0/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="E:\Unreal Engine\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "E:\Unreal Engine\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T cs_6_6 -E SimulateMainComputeCS -Fc NiagaraEmitterInstanceShader.d3dasm -Fo NiagaraEmitterInstanceShader.dxil NiagaraEmitterInstanceShader
-:END
-PAUSE
